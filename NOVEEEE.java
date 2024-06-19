59dc9fe5ac8dec55514504e9c0dae44e2762be0f
Lucas <162452756+LuwcasGbs@users.noreply.github.com>
2024-06-19 10:43:35 -0300

    Update NOVEEEE.java

--- NOVEEEE.java
+++ NOVEEEE.java
@@ -3,8 +3,23 @@ package Aula1206;
 public class NOVEEEE {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner ler = new Scanner(System.in);
+		String MaisnovoNom = "";
+		int MaisnovoIda = Integer.MAX_VALUE;
+		int num = 0;
+		while (num < 10) {
+			System.out.print("Digite o nome da pessoa " + (num + 1) + ": ");
+			String nome = ler.nextLine();
+			System.out.print("Digite a idade da pessoa " + (num + 1) + ": ");
+			int idade = ler.nextInt();
+			ler.nextLine();
+			if (idade < MaisnovoIda) {
+				MaisnovoIda = idade;
+				MaisnovoNom = nome;
+			}
+			num++;
+		}
+		System.out.println("A pessoa mais nova é " + MaisnovoNom + ", com " + MaisnovoIda + " anos.");
+		ler.close();
 	}
-
 }
